aa5bf3e779c3eff4d14f015a62691a9b78cae6e1
Anthony Lim <alim3@hawki.iit.edu>
2021-03-12 10:06:45 -0600

    Initial Commit

---
+++ Shared/RungeKuttaShrodingerEqApp.swift
@@ -0,0 +1,17 @@
+//
+//  RungeKuttaShrodingerEqApp.swift
+//  Shared
+//
+//  Created by anthony lim on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct RungeKuttaShrodingerEqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
